57ae9e127294528bd030c277c3b3cee35846e7e7
Harjaspal Singh <71326879+Harjas-Sidhu@users.noreply.github.com>
2022-11-23 22:59:07 +0530

    Update Project 6.py

--- Project 6.py
+++ Project 6.py
@@ -1,51 +1,73 @@
-import os
 import time
+import os
 
-acronym_list = []
-
-def acronym(string):
-    acronym = string[0]
-    for i in range(1, len(string)):
-        if(string[i-1] == ' '):
-            acronym += string[i]
-    return acronym.upper()
+name_list = []
+contact_list = []
 
 while(True):
-    os.system('cls')
-    choice = int(input("""
-    |-----------------------------------------------------------------|
-    |  1. Make another Acronym                                        |
-    |-----------------------------------------------------------------|
-    |  0. Quit                                                        |
-    |-----------------------------------------------------------------|\n\n"""))
-    time.sleep(0.5)
+    print("\n     ---------------     1. Add a New Contact        ---------------\n")
+    print("     ---------------     2. View a Contact           ---------------\n")
+    print("     ---------------     3. Display all Contacts     ---------------\n")
+    print("     ---------------     0. Exit                     ---------------\n")
+    choice = int(input("Enter Your Choice: "))
+    time.sleep(0.25)
     os.system('cls')
     if(choice == 1):
-        string = str(input("""
-        |-----------------------------------------------------------------|
-        |           Enter Your Desired String                             |
-        |-----------------------------------------------------------------|\n\n"""))
-        acronym_list.append(acronym(string))
-
+        name = str(input("     Enter The Name    \n\t"))
+        contact = int(input("     Enter Contact Number     \n\t"))
+        name_list.append(name)
+        contact_list.append(contact)
+        os.system('cls')
+    elif(choice == 2):
+        print("\n     ---------------     1. Search by Name        ---------------\n")
+        print("     ---------------     0. Search by Contact     ---------------\n")
+        usr_choice = int(input("Enter Your Choice: "))
+        time.sleep(0.25)
+        os.system('cls')
+        if(usr_choice == 1):
+            name = str(input("      Enter the Name of Contact You want to Search     \n\t"))
+            if(name in  name_list):
+                index = name_list.index(name)
+                os.system('cls')
+                print("---     ", name_list[index], " : ", contact_list[index], "     ---\n")
+                time.sleep(1)
+                os.system('cls')
+            else:
+                os.system('cls')
+                print("\n     --------------     Sorry, Name Not Found     ---------------\n")
+                time.sleep(0.25)
+                os.system('cls')
+        elif(usr_choice == 0):
+            contact = int(input("      Enter the Name of Contact You want to Search     \n\t"))
+            if(contact in  contact_list):
+                index = contact_list.index(contact)
+                os.system('cls')
+                print("---     ", name_list[index], " : ", contact_list[index], "     ---\n")
+                time.sleep(1)
+                os.system('cls')
+            else:
+                os.system('cls')
+                print("\n     --------------     Sorry, Contact Not Found     ---------------\n")
+                time.sleep(0.25)
+                os.system('cls')
+        else:
+            os.system('cls')
+            print("\n     ---------------     INVALID  CHOICE     ---------------\n")
+            time.sleep(0.25)
+            os.system('cls')
+    elif(choice == 3):
+        print("\n     ---------------     Contacts     ----------------\n")
+        for index in range(0, len(name_list)):
+            print("---     ", name_list[index], " : ", contact_list[index], "     ---\n")
+            time.sleep(1)
     elif(choice == 0):
+        print("\n     ---------------     Thanks For Using Our Software!!!     ---------------\n")
         break
-
     else:
-        print("""
-        |-----------------------------------------------------------------|
-        |                        Invalid Choice!!!!!!!!!                  |
-        |-----------------------------------------------------------------|""")
-        time.sleep(2)
         os.system('cls')
-        print("""
-        |-----------------------------------------------------------------|
-        |                        Restarting.............                  |
-        |-----------------------------------------------------------------|""")
-        time.sleep(3)
+        print("\n     ---------------     INVALID  CHOICE     ---------------\n")
+        time.sleep(0.25)
         os.system('cls')
-
-for i in list(acronym_list):
-     print("""
-        |-----------------------------------------------------------------|
-        |                        """, i, """                                    |
-        |-----------------------------------------------------------------|""", end='')
+        print("\n     ---------------     RESTARTING.........     ---------------\n")
+        time.sleep(0.25)
+        os.system('cls')
